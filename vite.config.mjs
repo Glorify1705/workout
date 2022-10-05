911f3322d3cabd56043754c896a01633fe5ce79c
Glorify1705 <github.9y8w2@simplelogin.com>
2022-10-05 22:05:59 +0200

    Update vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,5 +4,11 @@ import createReScriptPlugin from '@jihchi/vite-plugin-rescript';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), createReScriptPlugin()],
+    plugins: [
+        reactRefresh({
+            include: "**/*.js",
+            exclude: "/node_modules/"
+        }),
+        createReScriptPlugin()
+    ],
 });
